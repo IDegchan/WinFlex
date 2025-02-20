1bc59f06ea2d8922a2c79e7b5c5efadb34e9cc0a
IDegchan <gamerolikam@gmail.com>
2025-02-20 16:37:06 +0200

    On branch main Your branch is behind 'origin/main' by 3 commits, and can be fast-forwarded.   (use "git pull" to update your local branch)
    Changes to be committed:
    	modified:   src/frontend/main.py

--- src/frontend/main.py
+++ src/frontend/main.py
@@ -1,6 +1,8 @@
 import customtkinter as ctk
 
 root = ctk.CTk()
+root.geometry("400x300")
+root.minsize(400, 300)
 root.title("WinFlex")
 
 tabview = ctk.CTkTabview(root)
@@ -9,12 +11,22 @@ tabview.pack(expand=True, fill="both")
 tabview.add("Frame 1")
 tabview.add("Frame 2")
 
-frame1 = ctk.CTkFrame(tabview.tab("Frame 1"), width=200, height=200)
-frame1.grid(row=0, column=0, padx=20, pady=20)
-frame1.configure(bg_color="red")
+frame1 = ctk.CTkFrame(tabview.tab("Frame 1"))
+frame1.pack(fill="both", expand=True, padx=10, pady=10)
 
-frame2 = ctk.CTkFrame(tabview.tab("Frame 2"), width=200, height=200)
-frame2.grid(row=0, column=0, padx=20, pady=20)
-frame2.configure(bg_color="blue")
+checkbox1 = ctk.CTkCheckBox(frame1, text="Чекбокс 1")
+checkbox1.pack(anchor="w", pady=5)
+
+checkbox2 = ctk.CTkCheckBox(frame1, text="Чекбокс 2")
+checkbox2.pack(anchor="w", pady=5)
+
+frame2 = ctk.CTkFrame(tabview.tab("Frame 2"))
+frame2.pack(fill="both", expand=True, padx=10, pady=10)
+
+checkbox3 = ctk.CTkCheckBox(frame2, text="Чекбокс 3")
+checkbox3.pack(anchor="w", pady=5)
+
+checkbox4 = ctk.CTkCheckBox(frame2, text="Чекбокс 4")
+checkbox4.pack(anchor="w", pady=5)
 
 root.mainloop()
