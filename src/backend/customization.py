46d45fab9ce1d33445a3a331efdfa0716d5545ba
IDegchan <gamerolikam@gmail.com>
2025-02-23 09:48:54 +0200

    On branch main Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	modified:   src/backend/customization.py

--- src/backend/customization.py
+++ src/backend/customization.py
@@ -10,19 +10,19 @@ class Highlight:
         self.r, self.g, self.b = red, green, blue
 
     @staticmethod
-    def set_color(key: str, value: str):
+    def _set_color(key: str, value: str):
         with winreg.OpenKey(winreg.HKEY_CURRENT_USER, Highlight.REG_PATH, 0, winreg.KEY_SET_VALUE) as regkey:
             winreg.SetValueEx(regkey, key, 0, winreg.REG_SZ, value)
         os.system("RUNDLL32.EXE user32.dll,UpdatePerUserSystemParameters")
 
     def change(self):
         adjust = lambda x: max(1, x - 20)
-        self.set_color("Hilight", f"{self.r} {self.g} {self.b}")
-        self.set_color("HotTrackingColor", f"{adjust(self.r)} {adjust(self.g)} {adjust(self.b)}")
+        self._set_color("Hilight", f"{self.r} {self.g} {self.b}")
+        self._set_color("HotTrackingColor", f"{adjust(self.r)} {adjust(self.g)} {adjust(self.b)}")
 
     def reset(self):
-        self.set_color("Hilight", self.DEFAULT_HILIGHT)
-        self.set_color("HotTrackingColor", self.DEFAULT_HOTTRACK)
+        self._set_color("Hilight", self.DEFAULT_HILIGHT)
+        self._set_color("HotTrackingColor", self.DEFAULT_HOTTRACK)
 
 if __name__ == "__main__":
     Highlight(0, 255, 115).change()
