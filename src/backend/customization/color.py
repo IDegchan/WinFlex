5dab20e932f523c1cf615e5f1489a7c2479eb1d3
IDegchan <gamerolikam@gmail.com>
2025-02-23 09:23:03 +0200

    On branch main Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	modified:   src/backend/customization/color.py

--- src/backend/customization/color.py
+++ src/backend/customization/color.py
@@ -1,15 +1,36 @@
 import winreg
 import os
 
-def change_highlight_color(red: int, green: int, blue: int):
-    key = r"Control Panel\Colors"
-    HilightColor = f"{red} {green} {blue}" #Default: 0 120 215
-    HotTrackingColor = f"{red-20} {green-20} {blue-20}" #Default: 0 102 204
+class Highlight:
+    def __init__(self, red: int, green: int, blue: int):
+        self.r = red
+        self.g = green
+        self.b = blue
 
-    with winreg.OpenKey(winreg.HKEY_CURRENT_USER, key, 0, winreg.KEY_SET_VALUE) as regkey:
-        winreg.SetValueEx(regkey, "Hilight", 0, winreg.REG_SZ, HilightColor)
-        winreg.SetValueEx(regkey, "HotTrackingColor", 0, winreg.REG_SZ, HotTrackingColor)
+    def change_highlight_color(self):
+        red = self.r
+        green = self.g
+        blue = self.b
+        HilightColor = f"{red} {green} {blue}"
 
-    os.system("RUNDLL32.EXE user32.dll,UpdatePerUserSystemParameters")
+        if self.r > 20: red -= 20
+        if self.g > 20: green -= 20
+        if self.b > 20: blue -= 20
 
-change_highlight_color(125, 125, 125)
+        HotTrackingColor = f"{red} {green} {blue}"
+
+        with winreg.OpenKey(winreg.HKEY_CURRENT_USER, r"Control Panel\Colors", 0, winreg.KEY_SET_VALUE) as regkey:
+            winreg.SetValueEx(regkey, "Hilight", 0, winreg.REG_SZ, HilightColor)
+            winreg.SetValueEx(regkey, "HotTrackingColor", 0, winreg.REG_SZ, HotTrackingColor)
+
+        os.system("RUNDLL32.EXE user32.dll,UpdatePerUserSystemParameters")
+        #os.system("shutdown -r -t 0")
+    
+    def reset(self):
+        HilightColor = f"0 120 215"
+        HotTrackingColor = f"0 102 204"
+        with winreg.OpenKey(winreg.HKEY_CURRENT_USER, r"Control Panel\Colors", 0, winreg.KEY_SET_VALUE) as regkey:
+            winreg.SetValueEx(regkey, "Hilight", 0, winreg.REG_SZ, HilightColor)
+            winreg.SetValueEx(regkey, "HotTrackingColor", 0, winreg.REG_SZ, HotTrackingColor)
+        
+        os.system("RUNDLL32.EXE user32.dll,UpdatePerUserSystemParameters")
