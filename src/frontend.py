68f84b315e57545c65eb897bcdff8762d7280ba1
IDegchan <gamerolikam@gmail.com>
2025-03-08 19:40:16 +0200

    Reload frontend

--- src/frontend.py
+++ src/frontend.py
@@ -1,27 +1,17 @@
-import os
 import customtkinter as ctk
+from backend import *
+
+Buttons = []
 
 ctk.set_appearance_mode("dark")
 ctk.set_default_color_theme("blue")
 
 root = ctk.CTk()
-root.geometry("400x300")
-root.minsize(400, 300)
+root.geometry("1000x650")
+root.minsize(800, 500)
 root.title("WinFlex")
 
-tabview = ctk.CTkTabview(root)
-tabview.pack(expand=True, fill="both")
-tabview.add("Explore")
-tabview.add("Settings")
-
-Explore = ctk.CTkFrame(tabview.tab("Explore"))
-Explore.pack(fill="both", expand=True, padx=10, pady=10)
-
-Settings = ctk.CTkFrame(tabview.tab("Settings"))
-Settings.pack(fill="both", expand=True, padx=10, pady=10)
-
-
-def create_tile(parent, title, description):
+def create_tile(parent, title, description, function):
     tile = ctk.CTkFrame(parent, corner_radius=10)
     tile.pack(pady=10, padx=10, fill="x")
 
@@ -31,11 +21,11 @@ def create_tile(parent, title, description):
     desc_label = ctk.CTkLabel(tile, text=description, font=("Arial", 12), wraplength=350)
     desc_label.pack(anchor="w", padx=10)
 
-    action_button = ctk.CTkButton(tile, text="Завантажити", compound="left")
-    action_button.pack(pady=10, padx=10, anchor="e")
+    Buttons.append(ctk.CTkButton(tile, text="Налаштувати", compound="left", command=function))
+    Buttons[-1].pack(pady=10, padx=10, anchor="e")
 
-create_tile(Explore, "Тема оформлення", "Змінити колірну схему Windows.")
-create_tile(Explore, "Іконки", "Налаштування системних іконок.")
-create_tile(Explore, "Анімації", "Управління анімаціями Windows.")
+create_tile(root, "Тема оформлення", "Змінити колірну схему оформлення", lambda: print("HELLO"))
+create_tile(root, "Провідник", "Mica блюр для Windows Explorer", lambda: print("HELL0"))
+create_tile(root, "Консоль", "Кастомізувати консоль", lambda: print("HELL"))
 
 root.mainloop()
